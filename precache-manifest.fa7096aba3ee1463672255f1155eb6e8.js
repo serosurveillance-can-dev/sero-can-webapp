8480125a7513a8a373dbc0163ffd00824459096c
Austin Atmaja <austin.atmaja@gmail.com>
2020-05-01 16:34:33 -0400

    Updates

---
+++ precache-manifest.fa7096aba3ee1463672255f1155eb6e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397e93a69dd030663bfba22b8d26839a",
+    "url": "/sero-can-webapp/index.html"
+  },
+  {
+    "revision": "89103520bc53623877f5",
+    "url": "/sero-can-webapp/static/css/main.1028f2e2.chunk.css"
+  },
+  {
+    "revision": "b9144f240f12514405f6",
+    "url": "/sero-can-webapp/static/js/2.9d7a82ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sero-can-webapp/static/js/2.9d7a82ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89103520bc53623877f5",
+    "url": "/sero-can-webapp/static/js/main.1b16d8da.chunk.js"
+  },
+  {
+    "revision": "d9bc157ed8995f8b83ce",
+    "url": "/sero-can-webapp/static/js/runtime-main.dcc9f6b0.js"
+  }
+]);
