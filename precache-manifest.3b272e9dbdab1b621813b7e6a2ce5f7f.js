c4f971b6be7dd83fa682b387c13f091c92d663ce
Ewan May <ewan.may@ucalgary.ca>
2020-05-03 18:21:40 -0600

    Updates

---
+++ precache-manifest.3b272e9dbdab1b621813b7e6a2ce5f7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "befce198d1aec2ef1ba8aa4e1ee2c65f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "734ce300f5ea5874389e",
+    "url": "/static/css/main.82894069.chunk.css"
+  },
+  {
+    "revision": "e5c2fddbed8ad87b8f3f",
+    "url": "/static/js/2.804406f0.chunk.js"
+  },
+  {
+    "revision": "b40c0a77070980eeb586bc85637014fd",
+    "url": "/static/js/2.804406f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "734ce300f5ea5874389e",
+    "url": "/static/js/main.e181a274.chunk.js"
+  },
+  {
+    "revision": "8a4ccd85fa1a8c123f6c",
+    "url": "/static/js/runtime-main.bcb6c0b9.js"
+  },
+  {
+    "revision": "fdcc9cf6b25ff3979e9dcad1158fed25",
+    "url": "/static/media/two-tone-dark.fdcc9cf6.svg"
+  },
+  {
+    "revision": "59be83b8e2e049ef32ab41833eedd5d1",
+    "url": "/static/media/two-tone-light.59be83b8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.758062599d1a6d76ffbd8a5b4b106c62.js"
+  "/precache-manifest.3b272e9dbdab1b621813b7e6a2ce5f7f.js"
 );
 
 self.addEventListener('message', (event) => {
