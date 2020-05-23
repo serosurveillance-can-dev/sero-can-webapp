ad945fbedb3e61254b96ade0ff926f120c8539ca
abeljoseph <38389364+abeljoseph@users.noreply.github.com>
2020-05-23 21:02:21 +0000

    Deploying to gh-pages from  @ 69813d7db46405e7c09720f42cb52b8ffca50d69 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ae3e4a407d47a1e94843f8ccb8e5e3c.js"
+  "https://serotracker.com/precache-manifest.66f4e340c9cdfc5a8ca62bd16a69958e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://serotracker.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
