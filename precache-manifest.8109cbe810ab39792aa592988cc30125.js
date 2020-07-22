07da2e387b54060f121041f7d60364427e57eafc
ewanmay <ewan.may@ucalgary.ca>
2020-07-22 05:53:44 +0000

    Deploying to gh-pages from  @ ad55ecdee0afb2978b49a68727467cd78476a18f 🚀

---
+++ precache-manifest.8109cbe810ab39792aa592988cc30125.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "807b8bb0a9260d948473ca3058793e56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "640c4779639a46ac9380",
+    "url": "/static/css/main.0d0f808a.chunk.css"
+  },
+  {
+    "revision": "d92751e49b89239a31fa",
+    "url": "/static/js/2.700eaad7.chunk.js"
+  },
+  {
+    "revision": "95bf16a127db85fe9a993bc81c380818",
+    "url": "/static/js/2.700eaad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640c4779639a46ac9380",
+    "url": "/static/js/main.aaa508e5.chunk.js"
+  },
+  {
+    "revision": "8a4ccd85fa1a8c123f6c",
+    "url": "/static/js/runtime-main.bcb6c0b9.js"
+  },
+  {
+    "revision": "2cac3c48459ff557e8e384490f69731e",
+    "url": "/static/media/IndustryUpdateJune10_2020.2cac3c48.png"
+  },
+  {
+    "revision": "4021b4239f7032bede7e7777d0c2228b",
+    "url": "/static/media/IndustryUpdateMay_2020.4021b423.png"
+  },
+  {
+    "revision": "a93736ac68df553221cc2bf85634aa70",
+    "url": "/static/media/IndustryUpdatePre_2020.a93736ac.png"
+  },
+  {
+    "revision": "0b43b193f3eb1f3db6868b3286234b31",
+    "url": "/static/media/Prepublication.0b43b193.png"
+  },
+  {
+    "revision": "ab2ccbe821d02a0d86771a5285753caf",
+    "url": "/static/media/colored-icon.ab2ccbe8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ec74d709e7bb70a10557bffef9b54ce.js"
+  "/precache-manifest.8109cbe810ab39792aa592988cc30125.js"
 );
 
 self.addEventListener('message', (event) => {
