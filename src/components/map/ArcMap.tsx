50ef5d5f3d71cbce146e587dba324c24bfca782d
Brett <brettdziedzic@gmail.com>
2021-01-28 17:28:22 -0700

    upgraded to WHO layers

--- src/components/map/ArcMap.tsx
+++ src/components/map/ArcMap.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import WebMap from '@arcgis/core/WebMap';
 import MapView from '@arcgis/core/views/MapView';
+import VectorTileLayer from '@arcgis/core/layers/VectorTileLayer'
 import '@arcgis/core/assets/esri/themes/dark/main.css';
 import config from '@arcgis/core/config.js';
 
 // Assets have been moved from the default location
 //config.assetsPath = '/public/assets';
-config.apiKey = "sY-5c37sab7Ua9fJqROLTY6lotogTPJRQBu4oCejYpSvFplZCeHIsLmpo9fjHLx2hJDJ1Iayr1UHgF2YUsudJuGWretEsDIeazhmo7ti13WoNZUb28uwv4uTe5HtWov_Xr4diJytg75SKMhD_guUgUREvC4HL7PHPBTVuSBu39tHVvSl5Sam4WEKO7xfswtWFTMwqNU1Nk0WdJsuVlSwHxHFC9UO9j9DoHUtE1AjwzE.";
+config.apiKey = "AAPKa8fc519357f34332a7101c24fd428bd7qrZNoFTp0NhABQN23dxAn-hSRRSwvkgKiN-CLOH--hru1Jd-9_mFM9LjxD4qIWdc";
 
 interface state {
     lng: number,
@@ -27,19 +28,33 @@ class ArcMap extends React.Component {
         };
     }
 
-
     componentDidMount() {
-        var map: WebMap;
-        map = new WebMap({
-            basemap: "topo-vector"
+        var map = new WebMap();
+
+        var view = new MapView({
+          container: "viewDiv",
+          map: map,
+          center: [-98.5795, 39.8282],
+          zoom: 3
         });
 
-        const view = new MapView({
-            map: map,
-            container: "viewDiv",
-            center: [-118.244, 34.052],
-            zoom: 12
+        var baseLayer = new VectorTileLayer({
+          url:
+            "https://tiles.arcgis.com/tiles/5T5nSi527N4F7luB/arcgis/rest/services/WHO_Polygon_Basemap_no_labels/VectorTileServer"
         });
+
+        var countryPolygonLayer = new VectorTileLayer({
+          url:
+            "https://tiles.arcgis.com/tiles/5T5nSi527N4F7luB/arcgis/rest/services/Countries/VectorTileServer"
+        });
+
+        var disputedLayer = new VectorTileLayer({
+          url:
+            "https://tiles.arcgis.com/tiles/5T5nSi527N4F7luB/arcgis/rest/services/WHO_Polygon_Basemap_Disputed_Areas_and_Borders_VTP/VectorTileServer"
+        });
+        map.add(baseLayer);
+        map.add(countryPolygonLayer);
+        map.add(disputedLayer);
     }
 
     render() {
@@ -50,99 +65,3 @@ class ArcMap extends React.Component {
 }
 
 export default ArcMap;
-
-// <div ref={(el) => (this.mapContainer = el ?? "")} className="mapContainer w-100" id="viewDiv"/>
-
-
-/*
-// Takes in the style JSON object from VectorTileServer API response
-// Modifies & adds attributes for Mapbox GL compatability
-async function prepare(url : string) {
-
-  const styleUrl = url+"/resources/styles/root.json";
-
-  const api = new httpClient()
-
-  let fetchedStyle = await api.getStyles(styleUrl);
-  fetchedStyle.sprite = url+"/resources/styles/../sprites/sprite";
-  fetchedStyle.glyphs = url+"/resources/styles/../fonts/{fontstack}/{range}.pbf";
-  fetchedStyle.sources.esri = {
-    type: 'vector',
-    tiles: [url+"/tile/{z}/{y}/{x}.pbf"],
-    maxzoom: 23,
-    };
-
-  return fetchedStyle;
-};
-
-function addEsriLayer(map : mapboxgl.Map, url : string){
-  prepare(url).then((style : mapboxgl.Style) => {
-    const layerGuid = "esri"+(Math.random().toString())
-    var source = style?.sources?.esri as mapboxgl.VectorSource;
-
-    //@ts-ignore
-    const l = style?.layers[0];
-    if(l?.id === "Countries")
-    {
-      source.promoteId = {"Countries": "CODE"}
-    }
-    map.addSource(layerGuid, source as mapboxgl.VectorSource);
-
-
-    style?.layers?.forEach((layer : any) => {
-      layer.source = layerGuid
-      map.addLayer(layer)
-    });
-  })
-}
-*/
-
-/*
-    var map : mapboxgl.Map;
-
-    // Sets WHO polygons as basemap
-    prepare("https://tiles.arcgis.com/tiles/5T5nSi527N4F7luB/arcgis/rest/services/WHO_Polygon_Basemap_no_labels/VectorTileServer").then((style) => {
-    map = new mapboxgl.Map({
-        container: this.mapContainer,
-        style: style,
-        center: [this.state.lng, this.state.lat],
-        zoom: this.state.zoom,
-      });
-
-    map.on('load', () => {
-      addEsriLayer(map, "https://tiles.arcgis.com/tiles/5T5nSi527N4F7luB/arcgis/rest/services/WHO_Polygon_Basemap_Disputed_Areas_and_Borders_VTP/VectorTileServer")
-      addEsriLayer(map, "https://tiles.arcgis.com/tiles/5T5nSi527N4F7luB/arcgis/rest/services/Countries/VectorTileServer")
-      var e = map.getSource('esri')
-      console.log(e)
-    })
-
-    var hoveredStateId : any = null;
-    // When the user moves their mouse over the state-fill layer, we'll update the
-    // feature state for the feature under the mouse.
-    map.on('mousemove', 'Land/1', function (e : any) {
-        var f = e?.features[0]?.layer
-        f.paint = {
-          "fill-color" : "rgba(255, 255, 0, 1)"
-        }
-        var layer = map.getLayer('Countries');
-        var layer2 = map.getLayer('Land/1');
-
-         var features2 = map.queryRenderedFeatures(undefined, { layers: ['Countries'] });
-         var features2 = map.queryRenderedFeatures(undefined, { layers: ['Land/1'] });
-         var features3 = map.querySourceFeatures('Land/1');
-         console.log(e)
-       });
-
-       // When the mouse leaves the state-fill layer, update the feature state of the
-       // previously hovered feature.
-       map.on('mouseleave', 'esri', function () {
-       if (hoveredStateId) {
-       map.setFeatureState(
-       { source: 'states', id: hoveredStateId },
-       { hover: false }
-       );
-       }
-       hoveredStateId = null;
-       });
-   })
-   */
