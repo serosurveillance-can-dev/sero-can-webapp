e0f267fa8841f08ac70078ae03fb23c566d274e2
annoyingpr <prannoylal@gmail.com>
2021-03-27 17:16:39 +0000

    Deploying to gh-pages from  @ 129689098e4560685926747069ab20701351711d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a1bd994e87e8f040e681e7f4a389a785.js"
+  "/precache-manifest.8729aafc0694ad39874d050de439cd99.js"
 );
 
 self.addEventListener('message', (event) => {
